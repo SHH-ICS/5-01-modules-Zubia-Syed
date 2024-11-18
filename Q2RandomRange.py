b936cc8c76a61d78ab39da02aa3561130bdb5acb
Zubia Syed <Zubia.syed@stu.ocsb.ca>
2024-11-18 15:47:06 +0000

    Python changes

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,11 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+import random 
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+
+random_number = random.uniform(a, b)
+print("Random number between", a, "and", b, "is:", random_number)
+
+
